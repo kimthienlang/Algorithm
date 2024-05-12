218be962e4f95131e9d0514893d8bb660ff8020c
Kim Thieen Lang <kimthienlang@gmail.com>
2024-05-13 01:00:49 +0700

    update: 19.RemoveNthNodeFromEndOfList.cpp

--- 19.RemoveNthNodeFromEndOfList.cpp
+++ 19.RemoveNthNodeFromEndOfList.cpp
@@ -12,15 +12,37 @@ struct ListNode {
 
 class Solution {
 public:
-    static ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* result;
-        return result;
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* result = new ListNode(0, head);
+        ListNode* dummy = result;
+
+        for (int i = 0; i < n; i++) {
+            head = head->next;
+            cout << "i: " << head->val << endl;
+        }
+
+        while (head != nullptr) {
+            dummy = dummy->next;
+            head = head->next;
+        }
+
+        dummy->next = dummy->next->next;
+        return result->next;        
     }
 };
-
 int main() {
-    ListNode head = ListNode();
+    ListNode *head = new ListNode(1, 
+                     new ListNode(2, 
+                     new ListNode(3, 
+                     new ListNode(4, 
+                     new ListNode(5)))));
     int n = 2;
-    Solution::removeNthFromEnd(&head, n);
+    Solution sol;
+    ListNode *result;
+    result = sol.removeNthFromEnd(head, n);
+    while(result != nullptr) {
+        cout << result->val << " ";
+        result = result->next;
+    }
     return 0;
 }
