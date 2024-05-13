ff19e80d8d156b8eaee78c3b292ab161bd720a14
Kim Thieen Lang <kimthienlang@gmail.com>
2024-05-13 08:12:00 +0700

    add: 22.generateParentheses.cpp

---
+++ 22.GenerateParentheses.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void backtrack(int n, int leftCount, int rightCount, vector<char>& output, vector<string>& result) {
+    if (leftCount >= n && rightCount >= n) {
+        string outputStr(output.begin(), output.end());
+        result.push_back(outputStr);
+    }
+
+    if (leftCount < n) {
+        output.push_back('(');
+        backtrack(n, leftCount + 1, rightCount, output, result);
+        output.pop_back();
+    }
+
+    if (rightCount < leftCount) {
+        output.push_back(')');
+        backtrack(n, leftCount, rightCount + 1, output, result);
+        output.pop_back();
+    }
+}
+
+vector<string> generateParenthesis(int n) {
+    vector<string> result;
+    vector<char> output;
+    backtrack(n, 0, 0, output, result);
+    return result;
+}
+
+int main() {
+    int n = 3;
+    vector<string> result = generateParenthesis(n);
+    for (auto x : result) {
+        cout << x << endl;
+    }
+    return 0;
+}
