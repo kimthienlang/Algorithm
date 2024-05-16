e198ee0beac4a61562beeba584b7a37db0dc2a4a
Kim Thieen Lang <kimthienlang@gmail.com>
2024-05-17 06:15:19 +0700

    add 30.SubstringWithConcatenationOfAllWords.cpp

--- 30.SubstringWithConcatenationOfAllWords.cpp
+++ 30.SubstringWithConcatenationOfAllWords.cpp
@@ -5,45 +5,45 @@
 using namespace std;
 
 vector<int> findSubstring(string s, vector<string>& words) {
-    vector<int> res;
-    int lenS = s.length();
-    int lenW = words.size();
-    int lenW0 = words[0].size();
+    unordered_map<string, int> freq, curr;
+    for (string& word : words) 
+        freq[word]++;
+    
+    int len = s.size();
+    int n = words.size();
+    int wordSize = words[0].size();
+    int windowSize = wordSize * n;
 
-    for (int i = 0; i < lenS; ++i) {
-        vector<int> indexW;
-        bool isExistJ[lenW];
-        memset(isExistJ, 0, sizeof(isExistJ));
-        int ii = i;
-
-        for (int k = 0; k < lenW; ++k)
-        for (int j = 0; j < lenW; ++j) {
-            bool checkJ = true;
-            if (isExistJ[j]) continue;
-            for (int l = 0; l < lenW0; ++l) {
-                if (words[j][l] != s[ii]) {
-                    checkJ = false;
-                    ii -= l;
+    vector<int> ans;
+    for (int startPos = 0; startPos < wordSize; ++startPos) {
+        int start = startPos;
+        while (start + windowSize - 1 < len) {
+            curr = freq;
+            string currWord;
+            bool match = true;
+            for (int j = 0; j < n; ++j) {
+                currWord = s.substr(start + j * wordSize, wordSize);
+                if (!curr.count(currWord) or curr[currWord] == 0){
+                    match = false;
                     break;
-                } else ii++;
+                }
+                curr[currWord]--;
             }
-            if (checkJ) {
-                indexW.push_back(j);
-                cout << i << "-" << j << endl;
-                isExistJ[j] = true;
+            if (match == true) {
+                ans.push_back(start);
             }
-        }
-        if (indexW.size() == lenW) res.push_back(i);
+            start += wordSize;
+        } 
     }
 
-    return res;
+    return ans;
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    string testcase1 = "barfoofoobarthefoobarman";
-    vector<string> word = {"bar","foo","the"};
+    string testcase1 = "b";
+    vector<string> word = {"bbb","bbb"};
 
     vector<int> result = findSubstring(testcase1, word);
 
