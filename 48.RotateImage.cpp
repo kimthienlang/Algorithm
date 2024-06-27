39af14f370460103eeb46e365f067861c907c0cd
Kim Thieen Lang <kimthienlang@gmail.com>
2024-06-27 23:24:38 +0700

    update

--- 48.RotateImage.cpp
+++ 48.RotateImage.cpp
@@ -11,6 +11,18 @@ void printMatrix(vector<vector<int>> matrix) {
     cout << endl;
 }
 
+void rotateNotAllocate(vector<vector<int>> matrix) {
+    int row = matrix.size();
+        for(int i=0;i<row; i++){
+            for(int j=0; j<=i;j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        for(int i=0;i<row;i++){
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+}
+
 void rotate(vector<vector<int>>& matrix) {
     vector<vector<int>> rtMatrix;
     int lenMatrix = matrix.size();
